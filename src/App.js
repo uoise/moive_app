ece7f56b0b61fb41ef8e03dae06eb4bd69a1eae4
uoise <yw823x@daum.net>
2021-06-25 15:41:23 +0900

    render movies

--- src/App.js
+++ src/App.js
@@ -1,50 +1,42 @@
+import React from "react";
+import axios  from "axios";
+import Movie from "./Movie";
 
-function Food({name, pics}){
-  return <div>
-    <h1>i like {name} </h1>
-    <img src={pics} alt={name}/>
-    </div>;
+class App extends React.Component{
+  state={
+    isLoading: true,
+    movies: []
+  };
+getMovies = async () => {
+  const {
+    data : {
+      data: { movies }
+      }
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+  this.setState({ movies, isLoading: false })
 }
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+  componentDidMount() {
+    this.getMovies();
   }
-];
+  
 
-function App() {
-  return (
-    <div> 
-      {foodILike.map(d => <Food key={d.id} name={d.name} pics={d.image}/>)}
-    </div>
-  );
+  render(){
+    const {isLoading, movies} = this.state;
+    return (
+    <div>{isLoading ? "Loading..." : movies.map(movie =>  
+    <Movie 
+    key={movie.id}
+    id={movie.id} 
+    year={movie.year} 
+    title={movie.title} 
+    summary={movie.summary} 
+    poster={movie.medium_cover_image}
+    />
+    )}
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App; 
