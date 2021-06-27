438f6d5d10faecffbe544df5fa3d4ed25c5f6adb
uoise <yw823x@daum.net>
2021-06-27 16:56:44 +0900

    mvapp done

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+    return (
+        <div className="about__container">
+            <span>
+            "LARGE TEXT”
+            </span>
+            <span>− small text, Namb00s</span>
+        </div>
+    );
+}
+
+export default About;
